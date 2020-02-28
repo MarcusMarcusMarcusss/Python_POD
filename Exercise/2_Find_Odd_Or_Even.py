5f15d495f6a16899d13326c3bb0ec7bb8d239203
MarcusMarcusMarcusss <53335641+MarcusMarcusMarcusss@users.noreply.github.com>
2020-02-28 15:04:30 +0800

    Create 2_Find_Odd_Or_Even.py

---
+++ Exercise/2_Find_Odd_Or_Even.py
@@ -0,0 +1,16 @@
+
+#reading user inputs and check if the number is odd or even
+#find last digit from the user input & first digit
+                                          #first digit
+                                          #digits = (int)(math.log10(userInput)) 
+                                          #firstDigit = (int)(userInput / pow(10, digits))
+# in this case i ll be using last digit
+userInput=int(input("Enter a number :"))
+getlastdigits=(userInput % 10)
+
+if  (userInput% 2 ) == 0:
+    print("{0} is Even".format(userInput))
+else:
+  print("{0} is Odd".format(userInput))
+
+print(getlastdigits,"is your last digit")
