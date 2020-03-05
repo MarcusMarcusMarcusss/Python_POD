a260ed7d3c8db07c420e19d15646ed200d8597ce
MarcusMarcusMarcusss <53335641+MarcusMarcusMarcusss@users.noreply.github.com>
2020-03-05 12:47:47 +0800

    Update IntToStringHex.py

--- Exercise/IntToStringHex.py
+++ Exercise/IntToStringHex.py
@@ -1,8 +1,7 @@
-/*
-Given a non-negative integer n, convert the integer to hexadecimal and return the result as a string. 
-Hexadecimal is a base 16 representation of a number, where the digits are 0123456789ABCDEF. 
-Do not use any builtin base conversion functions like hex.
-*/
+#Given a non-negative integer n, convert the integer to hexadecimal and return the result as a string. 
+#Hexadecimal is a base 16 representation of a number, where the digits are 0123456789ABCDEF. 
+#Do not use any builtin base conversion functions like hex.
+
 
 def to_hex(n):
   return "%X" % int(n)
